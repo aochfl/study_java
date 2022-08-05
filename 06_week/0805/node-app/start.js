e4b733d4c997dbbbea1e706d04950584eb758798
Jinyeong <aochfl_@naver.com>
2022-08-05 14:31:22 +0900

    node 기본세팅

---
+++ 06_week/0805/node-app/start.js
@@ -0,0 +1,14 @@
+/*
+    1. Node install
+        - node.js.org -> Lts 버전 다운로드 -> 설치
+        - 윈도우 +r => cmd또는 vscode Ctrl+백틱
+        - node --version : 설치확인
+
+    2. npm init -y : 사용 프로그램을 관리하는 파일만들기
+
+    3. node 필요한 프로그램은 명령어를 복사해서 설치
+        - 필요한 프로그램 설치는 vscode Ctrl + 백틱
+        - npmjs.com에서 필요한 프로그램 검색 후 설치
+
+    4. npm i : dependencies 파일 설치
+*/
